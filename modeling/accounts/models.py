cce62829643303b7a7042e6e9d8b70cb61deab52
배용균 <lsdtve@gmail.com>
2020-10-20 17:49:39 +0900

    S03P31C202-17 | user profile modeling

--- modeling/accounts/models.py
+++ modeling/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_first = models.IntegerField(default=0)
+
+
+class UserProfile(models.Model):
+    # 이미지경로 나중에 추가 필요함
+    profile_img = models.ImageField(blank=True,null=True)
