37aef826967a2729a712efd4ba57d68a3a94558b
배용균 <lsdtve@gmail.com>
2020-10-21 02:54:53 +0900

    S03P31C202-20 | program modeling

--- modeling/api/models.py
+++ modeling/api/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from accounts.models import Trainer
 # Create your models here.
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+class Program(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name="program")
+    price = models.IntegerField()
+    visit_count = models.IntegerField(default=0)
