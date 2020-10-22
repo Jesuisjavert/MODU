45d3e1bf98eaf0a2ad13adbda911dc56696bf5b8
배용균 <lsdtve@gmail.com>
2020-10-22 15:37:34 +0900

    front kakao login test

--- modeling/frontend/src/main.js
+++ modeling/frontend/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+window.Kakao.init('abde304aa1e26779e03c975a6d6035de');
 new Vue({
   router,
   store,
--- modeling/frontend/src/views/Home.vue
+++ modeling/frontend/src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>kakao login</h1>
+    <button @click="kakaoLogin"></button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+    export default {
+        methods: {
+            kakaoLogin() {
+                window.Kakao.Auth.login({
+                    scope : 'profile, account_email',
+                    success: this.GetMe,
+                });
+            },
+            async GetMe(authObj){
+                console.log(authObj);
+                window.Kakao.API.request({
+                    url:'/v2/user/me',
+                    success : res => {
+                        const kakao_account = res.kakao_account;
+                        console.log(kakao_account)
+                        const userInfo = {
+                            nickname : kakao_account.profile.nickname,
+                            email : kakao_account.email,
+                            password : '',
+                            account_type : 2,
+                        }
+                        console.log(userInfo)
+                        let accessToken = authObj.access_token
+                        let form = new FormData()
+                        form.append('access_token', accessToken)
+                        axios.post('http://127.0.0.1:8000/api/rest-auth/kakao/login/',form)
+                        .then((res)=>{
+                          console.log(res)
+                        })
+                        .catch((err)=>{
+                          console.log(err)
+                        })
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+                    },
+                    fail : error => {
+                        console.log(error);
+                    }
+                })
+            }
+        }
+    }
 </script>
