220dadcfc458b4420722a72d86e9d0e9358cd7b6
배용균 <lsdtve@gmail.com>
2020-10-26 22:52:58 +0900

    S03P31C202-48 | gym detail GET

--- modeling/api/urls.py
+++ modeling/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('tag/', views.TagView.as_view(), name="tag"),
     path('gym/', views.GymView.as_view(), name="gym"),
+    path('gym/<int:pk>', views.GymDetailView.as_view(), name="gymdetail")
 ]
--- modeling/api/views/gym_views.py
+++ modeling/api/views/gym_views.py
@@ -1,7 +1,19 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from accounts.models import Gym
 from rest_framework import generics
+from rest_framework.views import APIView
 from ..serializers import GymSerializer
 
 class GymView(generics.ListCreateAPIView):
     queryset = Gym.objects.all()
     serializer_class = GymSerializer
+
+class GymDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Gym, pk=pk)
+      
+    def get(self, request, pk, format=None):
+        gym = self.get_object(pk)
+        serializer = GymSerializer(gym)
+        return Response(serializer.data)
