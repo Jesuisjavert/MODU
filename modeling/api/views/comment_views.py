9fa961aa822483ab72be8e7f3946c51caf3582f4
배용균 <lsdtve@gmail.com>
2020-10-27 02:58:44 +0900

    S03P31C202-42 | trainercomment GET

--- modeling/api/views/comment_views.py
+++ modeling/api/views/comment_views.py
@@ -22,3 +22,7 @@ class TrainerDetailView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TrainerCommentView(generics.ListCreateAPIView):
+    queryset = TrainerComment
+    serializer_class = TrainerComment
