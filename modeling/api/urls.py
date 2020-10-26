e6fce8959f0a6fc008b766c2a13efaf733c4d8db
배용균 <lsdtve@gmail.com>
2020-10-27 01:49:38 +0900

    S03P31C202-55 | trainer detail create url

--- modeling/api/urls.py
+++ modeling/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('gym/', views.GymView.as_view(), name="gym"),
     path('gym/<int:pk>/', views.GymDetailView.as_view(), name="gymdetail"),
     path('trainer/', views.TrainerView.as_view(), name="trainer"),
+    path('trainer/<int:pk>/', views.TrainerDetailView.as_view(), name="trainerdetail"),
     # path('trainer/<int:pk>/comment/', views.TrainerCommentView.as_view(), name="trainercomment"),
 ]
