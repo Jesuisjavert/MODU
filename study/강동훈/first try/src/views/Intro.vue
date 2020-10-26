9e8c898112631564114d988fa6f7b7f9c0e279d4
강동훈 <jesuisjavert@gmail.com>
2020-10-26 22:06:12 +0900

    S03P31C202-9 | Router 설정 및 Trainer / Trainee Tab나누기

---
+++ study/강동훈/first try/src/views/Intro.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Intro</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Intro</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <ExploreContainer name="Intro page" />
+      <a href="/tabs/tab1"> Trainer</a>
+      <a href="/tabs/tab5"> Trainee</a>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  {
+  name: 'Tab8',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+</script>
---
+++ study/강동훈/first try/src/views/Tabs2.vue
@@ -0,0 +1,36 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+        <ion-icon name="body-outline" />
+        <ion-label>Tab 5</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab6" href="/tabs/tab6">
+          <ion-icon name="calendar-outline" />
+          <ion-label>Tab 6</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab7" href="/tabs/tab7">
+          <ion-icon name="chatbubbles-outline" />
+          <ion-label>Tab 7</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab8" href="/tabs/tab8">
+          <ion-icon name="apps-outline" />
+          <ion-label>Tab 8</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
+
+export default {
+  name: 'Tabs',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+}
+</script>
