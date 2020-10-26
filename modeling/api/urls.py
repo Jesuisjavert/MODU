abe1e6c692a6d46c65e11367311cf258e8c2da7a
배용균 <lsdtve@gmail.com>
2020-10-27 01:13:21 +0900

    S03P31C202-42 | trainer comment create url

--- modeling/api/urls.py
+++ modeling/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('tag/', views.TagView.as_view(), name="tag"),
     path('gym/', views.GymView.as_view(), name="gym"),
-    path('gym/<int:pk>/', views.GymDetailView.as_view(), name="gymdetail")
+    path('gym/<int:pk>/', views.GymDetailView.as_view(), name="gymdetail"),
+    path('trainer/<int:pk>/comment/', views.TrainerComment.as_view(), name="trainercomment"),
 ]
