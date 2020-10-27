4966474e1d7e1c171519ee22ca21d7ea4ba161ca
배용균 <lsdtve@gmail.com>
2020-10-27 17:45:15 +0900

    S03P31C202-39 | user is_frist PUT

--- modeling/accounts/views.py
+++ modeling/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -25,6 +26,11 @@ class UserInfo(APIView):
         except:
             return Http404
 
+    def put_user(self, is_first):
+        user = self.get_object()
+        user.is_first = is_first
+        user.save()
+
     def get(self, request):
         user = self.get_object()
         if user.is_first==1:
@@ -37,3 +43,10 @@ class UserInfo(APIView):
             print('초기유저')
             serializer = UserSerializers(user)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        if request.data['is_frist']=='1':
+            self.put_user(1)
+        else:
+            self.put_user(2)
+        return Response({"data": True})
