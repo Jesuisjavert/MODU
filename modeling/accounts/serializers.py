32fba604572874f40799d097c36b01bb05a8c9c1
배용균 <lsdtve@gmail.com>
2020-10-30 17:03:48 +0900

    S03P31C202-38 | user serializer create

--- modeling/accounts/serializers.py
+++ modeling/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Trainer, Client
+from .models import Trainer, Client, UserProfile
 
 User = get_user_model()
 
@@ -18,3 +18,15 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         exclude = ('user','tags')
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    profile_img = serializers.ImageField()
+    image_url =  serializers.SerializerMethodField(read_only=True)
+    def get_image_url(self,userprofile):
+
+        userimage = str(userprofile.profile_img)
+
+        return 'http://d3v9ilm5vhs4go.cloudfront.net/media/'+userimage
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
