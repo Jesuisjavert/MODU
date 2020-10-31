2ec761a865af426d61a150da78b89789d0503e14
배용균 <lsdtve@gmail.com>
2020-10-31 15:55:48 +0900

    front kakao pay test

--- modeling/frontend/src/views/Mypage.vue
+++ modeling/frontend/src/views/Mypage.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    <h1>Mypage</h1>
-  </div>
+    <div>
+        <button @click="pay">카카오페이로 결제하기</button>
+    </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
+
 export default {
-  methods: {
-    test() {
+    name : 'Mypage',
+    data: () => ({
+      dialog: false,
+      items: [5000, 10000, 20000],
+      value:'뭐지이건'
+    }),
+    methods:{
+        pay(){
+            let baseUrl = "http://127.0.0.1:8000/"
+            let form = new FormData()
+            form.append('amount', this.value)
+            axios.post(baseUrl+"api/kakaopay/", form)
+            .then((res)=>{
+                console.log(res)
+            })
+        }
     }
-  }
 }
 </script>
