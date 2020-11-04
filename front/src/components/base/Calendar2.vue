58fed1cb248f42a25e473694dd2024b3f92800b8
강병국 <kang9305@naver.com>
2020-11-05 01:13:40 +0900

    toast calendar, so hard.. I'm frustrated

--- front/src/components/base/Calendar2.vue
+++ front/src/components/base/Calendar2.vue
@@ -16,66 +16,26 @@
               class="btn btn-default"
               data-toggle="modal"
             >
-              New schedule
+              새 일정
             </button>
           </div>
-          <span class="dropdown">
-            <button
-              id="dropdownMenu-calendarType"
-              class="btn btn-default btn-sm dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="true"
-            >
-              <i
-                id="calendarTypeIcon"
-                class="calendar-icon ic_view_month"
-                style="margin-right: 4px;"
-              ></i>
-              <span id="calendarTypeName">Dropdown</span>&nbsp;
-              <i class="calendar-icon tui-full-calendar-dropdown-arrow"></i>
-            </button>
-            <ul
-              class="dropdown-menu"
-              role="menu"
-              aria-labelledby="dropdownMenu-calendarType"
-            >
-               <li role="presentation">
-                <a
-                  class="dropdown-menu-title"
-                  role="menuitem"
-                  data-action="toggle-monthly"
-                >
-                  <i class="calendar-icon ic_view_month"></i>Month
-                </a>
-              </li>
-              <li role="presentation">
-                <a
-                  class="dropdown-menu-title"
-                  role="menuitem"
-                  data-action="toggle-weekly"
-                >
-                  <i class="calendar-icon ic_view_week"></i>Weekly
-                </a>
-              </li>
-              <li role="presentation">
-                <a
-                  class="dropdown-menu-title"
-                  role="menuitem"
-                  data-action="toggle-daily"
-                >
-                  <i class="calendar-icon ic_view_day"></i>Daily
-                </a>
-              </li>
-            </ul>
-          </span>
+          <div>
+            {{ getTime }}
+          </div>
+          <div class="dropdown btn">
+            <select name="option" v-model="view">
+              <option value="month" selected>Month</option>
+              <option value="week">Week</option>
+              <option value="day">Day</option>
+            </select>
+          </div>
+
           <span id="menu-navi">
-            <button type="button" class="btn btn-default btn-sm move-today" data-action="move-today" @click="moveToTday()">Today</button>
-            <button type="button" class="btn btn-default btn-sm move-day" data-action="move-prev"  @click="moveToNextOrPrevRange(-1)">
+            <button type="button" class="btn btn-today" data-action="move-today" @click="moveToTday()">Today</button>
+            <button type="button" class="btn btn-move" data-action="move-prev"  @click="moveToNextOrPrevRange(-1)">
               <i class="fas fa-angle-left" data-action="move-prev"></i>
             </button>
-            <button type="button" class="btn btn-default btn-sm move-day" data-action="move-next" @click="moveToNextOrPrevRange(1)">
+            <button type="button" class="btn btn-move" data-action="move-next" @click="moveToNextOrPrevRange(1)">
               <i class="fas fa-angle-right" data-action="move-next"></i>
             </button>
           </span>
@@ -189,9 +149,10 @@ export default {
           },
           useCreationPopup: true,
           useDetailPopup: false,
+          getTime:"",
       }
   },
-  methods : {
+  methods: {
     moveToNextOrPrevRange(val) {
       if (val === -1) {
       this.$refs.tuiCalendar.invoke('prev');
@@ -202,12 +163,65 @@ export default {
     moveToTday() {
       this.$refs.tuiCalendar.invoke('today');
     },
-  }
+    setRenderRangeText(cal) {
+      var renderRange = document.getElementById('renderRange');
+      var options = cal.getOptions();
+      var viewName = cal.getViewName();
+      var html = [];
+      if (viewName === 'day') {
+          html.push(moment(cal.getDate().getTime()).format('YYYY.MM.DD'));
+      } else if (viewName === 'month' &&
+          (!options.month.visibleWeeksCount || options.month.visibleWeeksCount > 4)) {
+          html.push(moment(cal.getDate().getTime()).format('YYYY.MM'));
+      } else {
+          html.push(moment(cal.getDateRangeStart().getTime()).format('YYYY.MM.DD'));
+          html.push(' ~ ');
+          html.push(moment(cal.getDateRangeEnd().getTime()).format(' MM.DD'));
+      }
+      renderRange.innerHTML = html.join('');
+    },
+  },
+
 }
 </script>
 
-<style>
+<style scoped>
 .client{
   border: solid 2px rgba(122, 122, 122, 0.2)
+}
+
+#menu {
+  display: flex;
+  margin-bottom: 12px;
+  align-items: center;
+}
+
+.dropdown {
+  padding: 5px 4px;
+  border: 1px solid #aaaaaa;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+.dropdown select {
+  padding: 0px 10px;
+}
+
+.btn {
+  border: 1px solid #aaaaaa;
+  padding: 6px 12px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 8px;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn-move {
+  width: 39px;
+  font-size: 18px;
+  border-radius: 50%;
 }
 </style>
