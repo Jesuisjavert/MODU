27470190b7f61e8dc40e1709cdd4099cfeb939aa
배용균 <lsdtve@gmail.com>
2020-11-05 17:01:16 +0900

    frontend Trainer detail

---
+++ modeling/frontend/src/views/Trainer/TrainerDetail.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		{{trainer}}
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+import constants from '@/api/constants'
+export default {
+  data(){
+    return {
+			constants,
+			comment: null,
+			trainer: null,
+    }
+	},
+	created() {
+		this.get_trainer()
+		this.get_comment()
+	},
+  methods: {
+		get_trainer() {
+			const pk = this.$route.params.pk
+      axios.get(`${this.constants.API_URL}trainer/${pk}/`)
+        .then((res)=>{
+          this.trainer = res.data
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+		},
+    get_comment() {
+			const pk = this.$route.params.pk
+      axios.get(`${this.constants.API_URL}trainer/${pk}/comment/`)
+        .then((res)=>{
+          this.comment = res.data
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
