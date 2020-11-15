169371bfd0890c72c21e3c2b74f6afdeb5132118
hyunsukRho <titiman444@gmail.com>
2020-11-16 01:15:38 +0900

    chat function create

--- front/src/components/base/TrainerMypageList.vue
+++ front/src/components/base/TrainerMypageList.vue
@@ -22,11 +22,26 @@
             <v-expansion-panel-content v-else-if="item.title == '이번달 수입'">
                 <RevenueGraph></RevenueGraph>
             </v-expansion-panel-content>
+            <v-expansion-panel-content v-else-if="item.title == '채팅 참가하기'">
+                <v-col v-for="(room, index) in chatRooms" :key="index">
+                    <v-row>
+                        <v-col cols="auto" class="mr-auto">
+                            <p>{{room.client_name}}</p>
+                        </v-col>
+                        <v-col cols="auto">
+                            <button @click="pushChat(index)">참가하기</button>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-expansion-panel-content>
         </v-expansion-panel>
     </v-expansion-panels>
 </template>
 
 <script>
+import axios from "axios";
+import constants from "@/api/constants";
+import { mapState } from 'vuex';
 export default {
     name: "TrainerMypage",
     components: {
@@ -35,6 +50,7 @@ export default {
     },
     data() {
         return {
+            chatRooms :[],
             items: [
                 {
                     title: '나의 프로그램',
@@ -47,7 +63,7 @@ export default {
                     title: '이번달 수입',
                 },
                 {
-                  title: '식단관리',
+                  title: '채팅 참가하기',
                 },
                 
                 {
@@ -62,10 +78,37 @@ export default {
             ],
         }
     },
+    computed : {
+        ...mapState(['userToken','username'])
+    },
+    created(){
+      this.getChattingRoom()
+    },
     methods: {
         goProgramCreate() {
             this.$router.push({ name: 'ProgramCreate' })
-        }
+        },
+        pushChat(index) {
+            let roomid = this.chatRooms[index].roomid.slice(2, -1);
+            sessionStorage.setItem("chatroomId", roomid);
+            this.$socket.emit("join", roomid, this.username);
+            this.$router.push({ name: "Chat" });
+            },
+        getChattingRoom() {
+            const Token = `Bearer ${this.userToken}`;
+            axios
+                .get(`${constants.API_URL}chat/`, {
+                headers: {
+                    Authorization: Token,
+                },
+                })
+                .then((res) => {
+                res.data.forEach((item) => {
+                    this.chatRooms.push(item);
+                });
+                console.log(res);
+                });
+            },
     },
 }
 </script>
