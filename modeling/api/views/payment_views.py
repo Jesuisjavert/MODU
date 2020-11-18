eabe8d406e80596cbd85c0280b3fb0825d2a5b54
배용균 <lsdtve@gmail.com>
2020-11-18 17:47:41 +0900

    pull

--- modeling/api/views/payment_views.py
+++ modeling/api/views/payment_views.py
@@ -12,6 +12,7 @@ class KakaoPay(APIView):
         program_id = request.POST.get('program_id')
         price_id = request.POST.get('price_id')
         print(program_id,price_id,'----')
+        print(front_site)
         url = "https://kapi.kakao.com"
         headers = {
             'Authorization': "KakaoAK " + "19ec65168ecd5968e1f8e5eca0a3ea3c",
@@ -34,9 +35,10 @@ class KakaoPay(APIView):
             # 결제취소했을때
             'cancel_url': f'{front_site}cancel/',
         }
+        print(params,'params')
         response = requests.post(url+"/v1/payment/ready", params=params, headers=headers)
+        print(response,'response')
         response = json.loads(response.text)
-        print(response.text)
         return Response(response)
 
 class KakaoPayApprove(APIView):
